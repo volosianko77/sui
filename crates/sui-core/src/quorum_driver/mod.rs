789f83414800b17cd0ed48013e9e96e3c2aecf14
William Smith <williamprincesmith@gmail.com>
2024-02-02 00:34:18 -0800

    Add TrafficController

--- crates/sui-core/src/quorum_driver/mod.rs
+++ crates/sui-core/src/quorum_driver/mod.rs
@@ -147,7 +147,7 @@ impl<A: Clone> QuorumDriver<A> {
             );
             return Ok(());
         }
-        self.backoff_and_enqueue(transaction, tx_cert, old_retry_times)
+        self.backoff_and_enqueue(transaction, tx_cert, old_retry_times, client_addr)
             .await
     }
 
@@ -157,6 +157,7 @@ impl<A: Clone> QuorumDriver<A> {
         transaction: Transaction,
         tx_cert: Option<CertifiedTransaction>,
         old_retry_times: u32,
+        client_addr: Option<SocketAddr>,
     ) -> SuiResult<()> {
         let next_retry_after =
             Instant::now() + Duration::from_millis(200 * u64::pow(2, old_retry_times));
@@ -843,7 +844,8 @@ where
                 spawn_monitored_task!(quorum_driver.backoff_and_enqueue(
                     transaction.clone(),
                     tx_cert,
-                    old_retry_times
+                    old_retry_times,
+                    client_addr,
                 ));
             }
             Some(qd_error) => {
